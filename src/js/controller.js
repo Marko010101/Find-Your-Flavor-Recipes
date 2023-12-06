aa0692865d582b0316240ad303010c763c8bfe84
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-12-06 15:59:28 +0400

    Finished main structure to follow the rest of the project

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,17 +1,10 @@
 import * as model from './model.js';
 import recipeView from './view/recipeView.js';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -32,12 +25,11 @@ const controlRecipes = async function () {
     // 2) rendering receipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
