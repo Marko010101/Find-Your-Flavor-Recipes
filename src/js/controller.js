9b7fb2e8d54431602dc02252f41c2c419007595b
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-12-12 18:59:48 +0400

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,109 +1,109 @@
-// import * as model from './model.js';
-// import recipeView from './view/recipeView.js';
-// import searchView from './view/searchView.js';
-// import resultsView from './view/resultsView.js';
-// import paginationView from './view/paginationView.js';
-// import bookmarksView from './view/bookmarksView.js';
+import * as model from './model.js';
+import recipeView from './view/recipeView.js';
+import searchView from './view/searchView.js';
+import resultsView from './view/resultsView.js';
+import paginationView from './view/paginationView.js';
+import bookmarksView from './view/bookmarksView.js';
 
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-// // if (module.hot) {
-// //   module.hot.accept();
-// // }
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
-// const controlRecipes = async function () {
-//   try {
-//     const id = window.location.hash.slice(1);
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
 
-//     if (!id) return;
+    if (!id) return;
 
-//     recipeView.renderSpinner();
+    recipeView.renderSpinner();
 
-//     // 0) Update results view to mark selected search results
-//     resultsView.update(model.getSearchResultsPage());
+    // 0) Update results view to mark selected search results
+    resultsView.update(model.getSearchResultsPage());
 
-//     // 1) Updating bookmarks view
-//     bookmarksView.update(model.state.bookmarks);
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
 
-//     // 2) Loading recipe
-//     await model.loadRecipe(id);
+    // 2) Loading recipe
+    await model.loadRecipe(id);
 
-//     // 3) rendering receipe
-//     recipeView.render(model.state.recipe);
-//   } catch (err) {
-//     recipeView.renderError();
-//     console.error(err);
-//   }
-// };
+    // 3) rendering receipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
+};
 
-// const controlSearchResults = async function () {
-//   try {
-//     resultsView.renderSpinner();
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
 
-//     // 1) Get search query
-//     const query = searchView.getQuery();
-//     if (!query) return;
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
 
-//     // 2) Load search results
-//     await model.loadSearchResults(query);
+    // 2) Load search results
+    await model.loadSearchResults(query);
 
-//     // 3) Render results
-//     resultsView.render(model.getSearchResultsPage());
-//     bookmarksView.update(model.state.addBookmarks);
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+    bookmarksView.update(model.state.addBookmarks);
 
-//     // 4) Render initial pagination buttons
-//     paginationView.render(model.state.search);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// const controlPagination = function (goToPage) {
-//   // 1) Render new results
-//   resultsView.render(model.getSearchResultsPage(goToPage));
+const controlPagination = function (goToPage) {
+  // 1) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
 
-//   // 2) Render NEW pagination buttons
-//   paginationView.render(model.state.search);
-// };
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
 
-// const controlServings = function (newServings) {
-//   // Update the recipe servings
-//   model.updateServings(newServings);
+const controlServings = function (newServings) {
+  // Update the recipe servings
+  model.updateServings(newServings);
 
-//   // Update the recipe view
-//   // recipeView.render(model.state.recipe);
-//   recipeView.update(model.state.recipe);
-// };
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
 
-// const controlAddBookmark = function () {
-//   // 1) Add/remove bookmark
-//   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
-//   else model.deleteBookmark(model.state.recipe.id);
+const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
 
-//   // 2) Update recipe view
-//   recipeView.update(model.state.recipe);
+  // 2) Update recipe view
+  recipeView.update(model.state.recipe);
 
-//   // 3) Render bookmarks
-//   bookmarksView.render(model.state.bookmarks);
-// };
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
 
-// const controlBookmarks = function () {
-//   bookmarksView.render(model.state.bookmarks);
-// };
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
 
-// const init = function () {
-//   bookmarksView.addHandlerRender(controlBookmarks);
-//   recipeView.addHandlerRender(controlRecipes);
-//   recipeView.addHandlerUpdateServings(controlServings);
-//   recipeView.addHandlerAddBookmark(controlAddBookmark);
-//   searchView.addHandlerSearch(controlSearchResults);
-//   paginationView.addHandlerClick(controlPagination);
-// };
-// init();
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
 
-// const clearBookmarks = function () {
-//   localStorage.clear('bookmarks');
-// };
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
 
-// // clearBookmarks();
+// clearBookmarks();
